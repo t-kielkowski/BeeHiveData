c0ce2c9227772da3dffb5844d5ca05ba5f45c88d
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-16 02:43:18 +0100

    removed classes

--- app/src/main/java/com/example/retrofitsample/model/Comments.kt
+++
@@ -1,9 +0,0 @@
-package com.example.retrofitsample.model
-
-data class Comments (
-    val postId : Long = 0,
-    val id : Long = 0,
-    val name : String = "",
-    val email : String = "",
-    val body : String = "",
-)
