690c6a830b159ebe51958d8970017e6dd68b6b24
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-16 02:44:57 +0100

    MainActivity

--- app/src/main/java/com/example/retrofitsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/retrofitsample/ui/MainActivity.kt
@@ -1,52 +1,56 @@
 package com.example.retrofitsample.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import com.example.retrofitsample.R
-import com.example.retrofitsample.model.Comments
-import com.example.retrofitsample.network.CommentRetriever
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import com.example.retrofitsample.chart.ChartPresenter
+import com.example.retrofitsample.network.BeehiveDataRetriever
+import com.github.mikephil.charting.charts.LineChart
 
 class MainActivity : AppCompatActivity() {
 
-    private val commentRetriever : CommentRetriever = CommentRetriever()
+    private val dataRetriever: BeehiveDataRetriever = BeehiveDataRetriever()
+    private val chartPresenter: ChartPresenter = ChartPresenter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initRecyclerview()
-        fetchComments()
+        udpateSpinnerList()
     }
 
-    private fun fetchComments() {
+    private fun udpateSpinnerList() {
 
-        val commentsFetchJob = Job()
+        val beeList = dataRetriever.fetchBeehiveList()
 
-        val errorHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-            throwable.printStackTrace()
-            Toast.makeText(this,"Error", Toast.LENGTH_SHORT).show()
+        var listOfBeeHives: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, beeList)
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+        spinner.adapter = listOfBeeHives
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val id = spinner.selectedItem.toString()
+
+                if (id == "UL_1") {
+                    setContentView(R.layout.temp_chart)
+
+                    var temp = dataRetriever.fetchBeehiveTemp(id)
+
+                    if (temp != null)
+                    {
+                        var lineChart : LineChart = findViewById(R.id.lineChart)
+                        chartPresenter.drawTempChart(temp, lineChart)
+                    }
+                }
+            }
         }
-
-        val scope = CoroutineScope(commentsFetchJob + Dispatchers.Main)
-
-        scope.launch(errorHandler){
-
-            //fetched data
-            val commentResponse = commentRetriever.getComments()
-
-            //render data in recyclerview
-            renderData(commentResponse)
-        }
-    }
-
-    private fun renderData(commentResponse: List<Comments>) {
-        recyclerview.adapter = CommentsAdapter(commentResponse = commentResponse)
-    }
-
-    private fun initRecyclerview() {
-        recyclerview.layoutManager = LinearLayoutManager(this)
     }
 }
